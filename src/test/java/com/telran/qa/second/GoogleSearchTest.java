df6a7b04610dd3b98e3ba737eca363c6824cf246
AntonStebelev <astebelev@gmail.com>
2021-03-25 21:33:18 +0200

    Create

--- src/test/java/com/telran/qa/second/GoogleSearchTest.java
+++ src/test/java/com/telran/qa/second/GoogleSearchTest.java
@@ -4,32 +4,65 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.util.concurrent.TimeUnit;
 
 public class GoogleSearchTest {
     WebDriver wd;
 
-    @Test
-    public void testGoogleSearch(){
+    @BeforeClass
+    public void setUp() {
         wd = new ChromeDriver();
         wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
         wd.get("https://www.google.com/");
+    }
 
-        wd.findElement(By.name("q")).click();
+    @Test
+    public void testGoogleSearch() {
+
+        click(By.name("q"));
         wd.findElement(By.name("q")).clear();
         wd.findElement(By.name("q")).sendKeys("WebDriver" + Keys.ENTER);
+        click(By.cssSelector("[href='https://www.selenium.dev/documentation/en/webdriver/'"));
+        wd.navigate().back();
+        /*Assert*/
+    }
 
-        wd.findElement(By.cssSelector("[href='https://www.selenium.dev/documentation/en/webdriver/'")).click();
+    @Test
+    public void testSearchImg() {
 
+        click(By.name("q"));
 
+        // wd.findElement(By.name("q")).click();
+        wd.findElement(By.name("q")).clear();
+        wd.findElement(By.name("q")).sendKeys("Java" + Keys.ENTER);
+        click(By.cssSelector("[data-hveid='CAEQAw']"));
+    }
 
+    public void click(By locator) {
+        wd.findElement(locator).click();
+    }
 
+    public int mult(int a, int b) {
+        return a * b;
+    }
+    public void type(By locator, String text){
+        click(locator);
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(text);
+    }
 
+    @Test(enabled = false)
+    public void myMult() {
+        int res = mult(2, 5);
+        System.out.println(res);
 
+    }
 
+    @AfterClass
+    public void tearDown() {
+        wd.quit();
     }
 
 
